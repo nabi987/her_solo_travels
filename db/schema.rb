e458b16bd47a3106a8748209032e4778ede66af9
Josh Newton <Joshnewton1999@gmail.com>
2024-10-14 16:57:46 +0100

    added trips model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_14_151824) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_14_155533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_14_151824) do
     t.index ["requestee_id"], name: "index_friendships_on_requestee_id"
     t.index ["requester_id", "requestee_id"], name: "index_friendships_on_requester_id_and_requestee_id", unique: true
     t.index ["requester_id"], name: "index_friendships_on_requester_id"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.string "name"
+    t.string "trip_image"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +53,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_14_151824) do
 
   add_foreign_key "friendships", "users", column: "requestee_id"
   add_foreign_key "friendships", "users", column: "requester_id"
+  add_foreign_key "trips", "users"
 end
