9cebbca71dfd6b852a5b9095f8ad225988966de2
Josh Newton <Joshnewton1999@gmail.com>
2024-10-14 17:46:02 +0100

    added locations cont and model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ApplicationRecord
   belongs_to :trip
+validates :country, :city, :start_date, :end_date, :activates, presence: true
+validates :description, length: {minimum: 5}
 end
