f4010b4e57376d07151bb0685ccd01d523ae6517
Josh Newton <Joshnewton1999@gmail.com>
2024-10-15 12:29:00 +0100

    added locations form and datepicker

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,25 @@
 class LocationsController < ApplicationController
+  def index
+    @location = Location.all
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    @location.save
+  end
+
+  private
+
+  def location_params
+    params.require(:trip).permit(:name, :photo, :user_id)
+  end
+
 end
