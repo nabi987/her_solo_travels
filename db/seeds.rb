4c36e7328a69849d72e816c1708389528fe887e0
Amreet Bains <amreet8@hotmail.co.uk>
2024-10-21 17:54:55 +0100

    seeded file and updated homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 require 'json'
 
 puts "cleaning the db...."
-#  Location.destroy_all
-#  Trip.destroy_all
-#  User.destroy_all
+Message.destroy_all
+Friendship.destroy_all
+Location.destroy_all
+Trip.destroy_all
+User.destroy_all
 puts "database clean!"
 puts "creating users..."
 
@@ -88,6 +90,7 @@ puts "creating users..."
 #   activites: "motorcross",
 #   trip: jungle
 # )
+require "cloudinary"
 require "open-uri"
 # Path to the JSON file
 file_path = Rails.root.join('db', 'seed_data.json')
@@ -105,25 +108,33 @@ users.each do |user_data|
     last_name: user_data['last_name'],
     age: user_data['age'],
     profile_bio: user_data['profile bio'],
-    #image_url: ,
+    # image_url: ,
     email: user_data['email'],
     password: BCrypt::Password.create(user_data['password'])
   )
 
-file = URI.parse(user_data['image_url']).open
-user.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-user.save
-end
+  if user_data['image_url'].present?
+    file = URI.parse(user_data['image_url']).open
+    # uploaded_file = Cloudinary::Uploader.upload(file)
+    user.photo.attach(io: file, filename: "#{user.first_name}_profile.png", content_type: "image/png")
+  end
 
+  user.save
+end
 
 susannah = User.find_by(first_name: "Susannah")
 
 brazil = Trip.create!(
   name: "Brazil 2025",
-  trip_image: "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: susannah
 )
+brazil_image = Cloudinary::Uploader.upload("https://images.unsplash.com/photo-1483729558449-99ef09a8c325?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 
+brazil.photo.attach(
+  io: URI.open(brazil_image['secure_url']),
+  filename: "brazil_trip.png",
+  content_type: brazil_image['format']
+)
 Location.create!(
   country: "Brazil",
   city: "Rio de Janeiro",
@@ -137,10 +148,14 @@ sabra = User.find_by(first_name: "Sabra")
 
 samerica = Trip.create!(
   name: "South America 2025!!",
-  trip_image: "https://images.unsplash.com/photo-1465513527097-544020a68b06?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: sabra
 )
-
+samerica_image = Cloudinary::Uploader.upload("https://images.unsplash.com/photo-1465513527097-544020a68b06?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+samerica.photo.attach(
+  io: URI.open(samerica_image['secure_url']),
+  filename: "samerica_trip.png",
+  content_type: samerica_image['format']
+)
 Location.create!(
   country: "Brazil",
   city: "Rio de Janeiro",
@@ -163,10 +178,14 @@ sumitra = User.find_by(first_name: "Sumitra")
 
 america = Trip.create!(
   name: "Americas 2025",
-  trip_image: "https://images.unsplash.com/photo-1643400814192-381f57499b42?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: sumitra
 )
-
+america_image = Cloudinary::Uploader.upload("https://images.unsplash.com/photo-1643400814192-381f57499b42?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+america.photo.attach(
+  io: URI.open(america_image['secure_url']),
+  filename: "america_trip.png",
+  content_type: america_image['format']
+)
 Location.create!(
   country: "Brazil",
   city: "Rio de Janeiro",
@@ -180,10 +199,15 @@ giulietta = User.find_by(first_name: "Giulietta")
 
 sm = Trip.create!(
   name: "South America'25",
-  trip_image: "https://images.unsplash.com/photo-1643400814192-381f57499b42?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: giulietta
 )
 
+sm_image = Cloudinary::Uploader.upload("https://images.unsplash.com/photo-1643400814192-381f57499b42?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+sm.photo.attach(
+  io: URI.open(sm_image['secure_url']),
+  filename: "sm_trip.png",
+  content_type: sm_image['format']
+)
 Location.create!(
   country: "Brazil",
   city: "Rio de Janeiro",
@@ -197,8 +221,14 @@ robinia = User.find_by(first_name: "Robinia")
 
 bali = Trip.create!(
   name: "Indonesia",
-  trip_image: "https://images.unsplash.com/photo-1643400814192-381f57499b42?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: robinia
+)
+
+bali_image = Cloudinary::Uploader.upload("https://images.unsplash.com/photo-1643400814192-381f57499b42?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+bali.photo.attach(
+  io: URI.open(bali_image['secure_url']),
+  filename: "bali_trip.png",
+  content_type: bali_image['format']
 )
 
 Location.create!(
@@ -209,3 +239,4 @@ Location.create!(
   activites: "Surfing and temples",
   trip: bali
 )
+puts "finished!"
