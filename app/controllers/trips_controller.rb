686b9eefa1881490b8c03383ece0ba02edd4b4c7
Amreet Bains <amreet8@hotmail.co.uk>
2024-10-22 14:14:45 +0100

    updated trip create method to attach random default image if user does not upload one when creating a trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,7 @@
 class TripsController < ApplicationController
+  require "cloudinary"
+  require "open-uri"
+
   def show
     @trip = Trip.find(params[:id])
     @location = Location.new
@@ -11,6 +14,9 @@ class TripsController < ApplicationController
   def create
     @trip = Trip.create(trip_params)
     @trip.user = current_user
+    unless trip_params[:photo].present?
+      random_default_image(@trip)
+    end
     if @trip.save
       redirect_to trip_path(@trip)
     else
@@ -22,5 +28,25 @@ class TripsController < ApplicationController
 
   def trip_params
     params.require(:trip).permit(:name, :photo, :image_url)
+  end
+
+  def random_default_image(trip)
+  default_images = ["https://images.unsplash.com/photo-1502301197179-65228ab57f78?q=80&w=2785&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/36/xIsiRLngSRWN02yA2BbK_submission-photo-7.jpg?q=80&w=2896&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1524850011238-e3d235c7d4c9?q=80&w=2928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1484627147104-f5197bcd6651?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1446160657592-4782fb76fb99?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1527610276295-f4c1b38decc5?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1519302039619-edad17063d15?q=80&w=1971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1509134072290-fffe152264c0?q=80&w=2946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1497634763913-2ea08bf9de5d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1506126613408-eca07ce68773?q=80&w=2202&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"]
+
+  trip_image = Cloudinary::Uploader.upload(default_images.sample)
+  trip.photo.attach(
+      io: URI.open(trip_image['secure_url']),
+      filename: "#{trip.name}_trip.png",
+      content_type: trip_image['format']
+    )
   end
 end
